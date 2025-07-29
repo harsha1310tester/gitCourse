3b9bd5bf0f560cf37658711b8d9d70aecb90769c
harsha <shriharsha1310@gmail.com>
2025-07-30 00:00:56 +0530

    new file added in feature branch

---
+++ newtypescriptfile.ts
@@ -0,0 +1,5 @@
+console.log("Hello, TypeScript!");
+let greeting: string = "Welcome to TypeScript!";    
+console.log(greeting);
+// This is a simple TypeScript file that logs messages to the console.
+//in feature branch
